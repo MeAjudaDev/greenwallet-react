e9bb872f163ecfb6d73c7879da71b996fccb7163
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-04-27 23:34:58 -0300

    chore: move components

---
+++ src/EditCategory.jsx
@@ -0,0 +1,23 @@
+import './EditCategory.css';
+
+function EditCategory() {
+    return (
+        <div className="container">
+            <h1>Editar Categoria De Despesas</h1>
+            <label id="description">
+                Descrição
+                <br />
+                <input type="input" placeholder="Alimentação" />
+            </label>
+            <label id="enabled">
+                Habilitado?
+                <br />
+                <input type="checkbox" />
+            </label>
+            <button className="buttonCancelar">Cancelar</button>
+            <button className="buttonSalvar">Salvar</button>
+        </div>
+    );
+}
+
+export default App;
