8c924047c4a1faa5d148a2c3938c88bca1817c74
stenioas <stenioas@gmail.com>
2021-05-01 17:11:15 -0300

    feature: register expenses
    update files

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import EditCategory from './EditCategory';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import EditCategory from "./EditCategory";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
     <React.StrictMode>
-        <EditCategory title="Nova Categoria De Despesas"/>
-        <EditCategory title="Editar Categoria De Despesas"/>
+        <EditCategory title="Nova Categoria De Despesas" />
+        <EditCategory title="Editar Categoria De Despesas" />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
 
 reportWebVitals();
