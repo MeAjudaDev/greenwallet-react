a420d735f91fe4de6b2607aca86c72ea52cf5668
stenioas <stenioas@gmail.com>
2021-05-01 17:09:15 -0300

    feature: register expenses
    type fieldset created

--- src/RegisterExpenses.jsx
+++ src/RegisterExpenses.jsx
@@ -1,12 +1,17 @@
-import Navbar from "./Navbar";
+import React from "react";
 
 function RegisterExpenses() {
+    const [type, setType] = React.useState("variavel");
+
+    const handleChange = ({ target }) => {
+        setType(target.value);
+    };
+
     return (
         <div>
             <header>
                 <span>Plataforma de Gestão Financeira Pessoal</span>
             </header>
-            <Navbar />
             <section className="">
                 <main>
                     <h1>Cadastrar Despesa</h1>
@@ -16,7 +21,7 @@ function RegisterExpenses() {
                     </div>
                     <form action="">
                         <fieldset>
-                            <label htmlFor="description">
+                            <label>
                                 <h2>Descrição</h2>
                                 <input
                                     type="text"
@@ -25,65 +30,100 @@ function RegisterExpenses() {
                                 />
                             </label>
                         </fieldset>
-                        <fieldset>
-                            <h2>Categoria</h2>
-                            <label htmlFor="alimentacao">
-                                <input
-                                    type="checkbox"
-                                    name="alimentacao"
-                                    value="alimentacao"
-                                />
-                                <span>Alimentação</span>
-                            </label>
-                            <label htmlFor="casa">
-                                <input
-                                    type="checkbox"
-                                    name="casa"
-                                    value="casa"
-                                />
-                                <span>Casa</span>
-                            </label>
-                            <label htmlFor="transporte">
-                                <input
-                                    type="checkbox"
-                                    name="transporte"
-                                    value="transporte"
-                                />
-                                <span>Transporte</span>
-                            </label>
-                            <label htmlFor="conhecimento">
-                                <input
-                                    type="checkbox"
-                                    name="conhecimento"
-                                    value="conhecimento"
-                                />
-                                <span>Conhecimento</span>
-                            </label>
-                            <label htmlFor="saude">
-                                <input
-                                    type="checkbox"
-                                    name="saude"
-                                    value="saude"
-                                />
-                                <span>Saúde</span>
-                            </label>
-                            <label htmlFor="lazer">
-                                <input
-                                    type="checkbox"
-                                    name="lazer"
-                                    value="lazer"
-                                />
-                                <span>Lazer</span>
-                            </label>
-                            <label htmlFor="diversos">
-                                <input
-                                    type="checkbox"
-                                    name="diversos"
-                                    value="diversos"
-                                />
-                                <span>Diversos</span>
-                            </label>
-                        </fieldset>
+                        <div>
+                            <fieldset>
+                                <h2>Categoria</h2>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="alimentacao"
+                                        value="alimentacao"
+                                    />
+                                    <span>Alimentação</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="casa"
+                                        value="casa"
+                                    />
+                                    <span>Casa</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="transporte"
+                                        value="transporte"
+                                    />
+                                    <span>Transporte</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="conhecimento"
+                                        value="conhecimento"
+                                    />
+                                    <span>Conhecimento</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="saude"
+                                        value="saude"
+                                    />
+                                    <span>Saúde</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="lazer"
+                                        value="lazer"
+                                    />
+                                    <span>Lazer</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="diversos"
+                                        value="diversos"
+                                    />
+                                    <span>Diversos</span>
+                                </label>
+                            </fieldset>
+                            <fieldset>
+                                <h2>Tipo</h2>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="grupo-radio"
+                                        value="variavel"
+                                        checked={type === "variavel"}
+                                        onChange={handleChange}
+                                    />
+                                    <span>Variável</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="grupo-radio"
+                                        value="fixo"
+                                        checked={type === "fixo"}
+                                        onChange={handleChange}
+                                    />
+                                    <span>Fixo</span>
+                                </label>
+                            </fieldset>
+                            <fieldset>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        value="com-vencimento"
+                                        checked
+                                    />
+                                    <span>Com vencimento</span>
+                                </label>
+                            </fieldset>
+                        </div>
                     </form>
                 </main>
             </section>
