39d01e6cd62563fcb474f8484fa3f4335c70f307
Iago Rodrigues <yagomello16@gmail.com>
2021-05-12 18:21:35 -0300

    wip(ListExpenses): started changes to new layout

--- src/ListExpenses.jsx
+++ src/ListExpenses.jsx
@@ -1,44 +1,41 @@
+import AppHeader from "./components/AppHeader";
+import NavBar from "./components/NavBar";
 import "./ListExpenses.css";
 
 export default function ListExpenses(props) {
+    const months = [
+        {
+            label: 'Janeiro',
+            link: '#',
+            selected: false
+        },
+        {
+            label: 'Fevereiro',
+            link: '#',
+            selected: false
+        },
+        {
+            label: 'Março',
+            link: '#',
+            selected: false
+        },
+        {
+            label: 'Abril',
+            link: '#',
+            selected: true
+        },
+        {
+            label: 'Maio',
+            link: '#',
+            selected: false
+        }
+    ]
+
     return (
-        <div className="expensesListWrapper">
-            <header className="expensesListHeader">
-                <div>
-                    <h3>{props.title}</h3>
-                    <h1>{props.group}</h1>
-                </div>
-
-                <button type="button" className="buttonListExpenses">
-                    Lançar Despesa
-                </button>
-            </header>
-
-            <ul className="expensesList">
-                <li className="expensesList__item">
-                    <div>
-                        <h2>Água</h2>
-                        <p>Lançado por Yonatha</p>
-                    </div>
-                    <span className="expenseValue">R$ 72,13</span>
-                </li>
-
-                <li className="expensesList__item">
-                    <div>
-                        <h2>Luz</h2>
-                        <p>Lançado por Daniel</p>
-                    </div>
-                    <span className="expenseValue">R$ 83,00</span>
-                </li>
-
-                <li className="expensesList__item">
-                    <div>
-                        <h2>Internet</h2>
-                        <p>Lançado por André</p>
-                    </div>
-                    <span className="expenseValue">R$ 90,99</span>
-                </li>
-            </ul>
-        </div>
+        <>
+            <AppHeader title="Casa" />
+            <NavBar items={months} />
+            <section className="expensesListWrapper"></section>
+        </>
     );
 }
