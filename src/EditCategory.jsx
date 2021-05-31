d5f6864c0454b557524bdc1aa470f8c2021e5162
angelomarques <anngelo.emanuel07@hotmail.com>
2021-05-31 20:31:29 -0300

    feat(EditCategory): criar e implementar api de obter categoria por id

--- src/EditCategory.jsx
+++ src/EditCategory.jsx
@@ -1,18 +1,60 @@
+import { useEffect, useState } from "react";
+import { getCategoryById } from "./api/v1/expenses";
 import "./EditCategory.css";
 
-function EditCategory(props) {
+function EditCategory({ id, title }) {
+    const [category, setCategory] = useState({});
+
+    useEffect(() => {
+        getCategoryById(id).then((category) => {
+            setCategory(category);
+        });
+    }, [id]);
+
+    function handleTextInputChange(event) {
+        const element = event.target;
+
+        setCategory((prevState) => {
+            setCategory({
+                ...prevState,
+                [element.name]: element.value,
+            });
+        });
+    }
+
+    function handleCheckboxChange(event) {
+        const element = event.target;
+
+        setCategory((prevState) => {
+            setCategory({
+                ...prevState,
+                state: element.checked ? "A" : "D",
+            });
+        });
+    }
+
     return (
         <div className="container">
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
             <label id="description">
                 Descrição
                 <br />
-                <input type="input" placeholder="Alimentação" />
+                <input
+                    onChange={handleTextInputChange}
+                    type="input"
+                    name="name"
+                    value={category?.name}
+                />
             </label>
             <label id="enabled">
                 Habilitado?
                 <br />
-                <input type="checkbox" />
+                <input
+                    onChange={handleCheckboxChange}
+                    checked={category?.state === "A"}
+                    name="state"
+                    type="checkbox"
+                />
             </label>
             <button className="buttonCancelar">Cancelar</button>
             <button className="buttonSalvar">Salvar</button>
