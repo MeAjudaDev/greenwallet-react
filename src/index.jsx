787f30f7c598dfc29ca2e74801574d059748b401
weescoelho <wes.coelho18@gmail.com>
2021-06-12 17:23:58 -0300

    refactor(index): fix components imports
    n

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import ExpenseForm from "./pages/ExpenseForm";
-import ListCategories from "./ListCategories";
+import ListCategories from "./pages/ListCategories";
 import ListExpenses from "./ListExpenses";
 import EditCategory from "./pages/EditCategory";
 
 ReactDOM.render(
     <React.StrictMode>
         <ExpenseForm title="Editar Despesa" isEditing={true} />
-        {/* <ExpenseForm title="Nova Despesa" />
+        <ExpenseForm title="Nova Despesa" />
         <ListCategories title="Despesas" />
         <ListExpenses title="Despesas" group="Casa" />
-        <EditCategory title="Editar Categoria" id={1} /> */}
+        <EditCategory title="Editar Categoria" id={1} />
     </React.StrictMode>,
     document.getElementById("root")
 );
