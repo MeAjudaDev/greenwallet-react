f421815670ab65cab2e89e62295806f503eb346f
stenioas <stenioas@gmail.com>
2021-06-15 01:12:47 -0300

    chore: root pseudo-class and var function was removed

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 import ExpenseForm from "./pages/ExpenseForm";
 import ListCategories from "./pages/ListCategories";
