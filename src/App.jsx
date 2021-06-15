060d8b16a8c890cb718013b8e1ca47ac9c074ef3
weescoelho <wes.coelho18@gmail.com>
2021-06-15 20:49:11 -0300

    feat(index and app): remove components on index.jsx, add routes and components on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch as Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import EditCategory from "./pages/EditCategory";
+import ExpenseForm from "./pages/ExpenseForm";
+import ListCategories from "./pages/ListCategories";
+import ListExpenses from "./pages/ListExpenses";
 
 function App() {
     return (
-        <div className="container"></div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/edit-category">
+                    <EditCategory title="Editar Categoria" id={1} />
+                </Route>
+                <Route path="/edit-expense">
+                    <ExpenseForm title="Editar Despesa" isEditing={true} />
+                </Route>
+                <Route path="/edit-expense">
+                    <ExpenseForm title="Nova Despesa" />
+                </Route>
+                <Route path="/list-category">
+                    <ListCategories title="Despesas" />
+                </Route>
+                <Route path="/list-expenses">
+                    <ListExpenses title="Despesas" group="Casa" />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
