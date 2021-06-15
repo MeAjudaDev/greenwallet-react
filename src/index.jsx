e1eeeecbeba3493848416f1d4ef838abc90b2b50
angelomarques <anngelo.emanuel07@hotmail.com>
2021-06-15 20:37:43 -0300

    refactor(index): remove index comments

--- src/index.jsx
+++ src/index.jsx
@@ -6,16 +6,16 @@ import ExpenseForm from "./pages/ExpenseForm";
 import ListCategories from "./pages/ListCategories";
 import ListExpenses from "./pages/ListExpenses";
 import EditCategory from "./pages/EditCategory";
-import Menu from './components/Menu';
+import Menu from "./components/Menu";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Menu/>
-        {/* <ExpenseForm title="Editar Despesa" isEditing={true} /> */}
-        {/* <ExpenseForm title="Nova Despesa" />
+        <Menu />
+        <ExpenseForm title="Editar Despesa" isEditing={true} />
+        <ExpenseForm title="Nova Despesa" />
         <ListCategories title="Despesas" />
         <ListExpenses title="Despesas" group="Casa" />
-        <EditCategory title="Editar Categoria" id={1} /> */}
+        <EditCategory title="Editar Categoria" id={1} />
     </React.StrictMode>,
     document.getElementById("root")
 );
