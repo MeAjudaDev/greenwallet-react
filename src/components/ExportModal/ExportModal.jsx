c846a3f9b78606cc5f0df1423eb87047e9310058
angelomarques <anngelo.emanuel07@hotmail.com>
2021-06-19 18:07:22 -0300

    feat(exportmodal): define modal structure

--- src/components/ExportModal/ExportModal.jsx
+++ src/components/ExportModal/ExportModal.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import styled from './ExportModal.module.scss'
+import React from "react";
+import styles from "./ExportModal.module.scss";
 
-
-export default function ExportModal (){
-  return(
-    <section className={styled.modalContainer}>
-      <div className={styled.modalWrapper}>
-        <h2>Export</h2>
-        <form action="">
-          <input type="date" />
-          <input type="date" />
-          <label htmlFor="fileFormat">Formato</label>
-          
-        </form>
-      </div>
-    </section>
-  )
+export default function ExportModal() {
+    return (
+        <section className={styles.modalContainer}>
+            <div className={styles.modalWrapper}>
+                <h2>Export</h2>
+                <form action="">
+                    <fieldset className={styles.dateWrapper}>
+                        <input type="date" />
+                        <input type="date" />
+                    </fieldset>
+                    <fieldset className={styles.radioWrapper}>
+                        <h3>Formato</h3>
+                        <label>
+                            <input type="radio" name="file-format" />
+                            CSV
+                        </label>
+                        <label>
+                            <input type="radio" name="file-format" />
+                            PDF
+                        </label>
+                    </fieldset>
+                    <fieldset className={styles.buttonWrapper}>
+                        <button type="button">Cancelar</button>
+                        <button type="submit">Exportar</button>
+                    </fieldset>
+                </form>
+            </div>
+        </section>
+    );
 }
