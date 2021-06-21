f8e845f42c0fe87413cff6525db9d38144951493
stenioas <stenioas@gmail.com>
2021-06-21 20:00:14 -0300

    feat(inputradio): component files created

---
+++ src/components/InputRadio/InputRadio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function InputRadio() {
+    return (
+        <>
+            <input type="radio" name="radio-group" checked />
+            <label for="test1">Apple</label>
+        </>
+    );
+}
