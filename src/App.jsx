18efbfd40ed866c89ea244ec1ea655f9c620851f
Iago Rodrigues <yagomello16@gmail.com>
2021-06-23 19:24:29 -0300

    style: remove unused Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch as Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch as Routes, Route } from "react-router-dom";
 import "./App.css";
 import EditCategory from "./pages/EditCategory";
 import ExpenseForm from "./pages/ExpenseForm";
