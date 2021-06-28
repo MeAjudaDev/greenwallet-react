8f0fd9d4c8f2fffc98e3f1f854597fb345e9a3e7
weescoelho <wes.coelho18@gmail.com>
2021-06-28 20:56:57 -0300

    feat: added classnames library and typing at props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,8 @@
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 import styles from "./Button.module.scss";
+import classNames from 'classnames/bind';
+
+let cx = classNames.bind(styles)
 
 export default function Button({
     children,
@@ -7,18 +10,31 @@ export default function Button({
     secondary,
     primary,
     textOnly,
+    outlined,
+    ...props
 }) {
     return (
-        <button className={styles.button} type="button">
+        <button
+            className={cx({
+                [styles.button]:true,
+                [styles.primary]:primary,
+                [styles.secondary]:secondary,
+                [styles.outlined]:outlined,
+                [styles.textOnly]:textOnly
+            })}
+            type={type}
+            {...props}
+        >
             {children}
         </button>
     );
 }
 
 Button.prototype = {
-    primary: propTypes.bool,
-    secondary: propTypes.bool,
-    textOnly: propTypes.bool,
-    type: propTypes.string.isRequired,
-    children: propTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
+    primary: PropTypes.bool,
+    secondary: PropTypes.bool,
+    textOnly: PropTypes.bool,
+    outlined: PropTypes.bool,
 };
