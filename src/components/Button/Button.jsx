55de8ccd0e399914b415cc43c57e160d29b6aebe
weescoelho <wes.coelho18@gmail.com>
2021-06-29 19:13:32 -0300

    feat: added default props and danger button type

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,40 +1,39 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.scss";
-import classNames from 'classnames/bind';
+import classNames from "classnames/bind";
 
-let cx = classNames.bind(styles)
+let cx = classNames.bind(styles);
 
-export default function Button({
-    children,
-    type,
-    secondary,
-    primary,
-    textOnly,
-    outlined,
-    ...props
-}) {
+export default function Button(props) {
     return (
         <button
             className={cx({
-                [styles.button]:true,
-                [styles.primary]:primary,
-                [styles.secondary]:secondary,
-                [styles.outlined]:outlined,
-                [styles.textOnly]:textOnly
+                [styles.button]: true,
+                [styles.primary]: props.primary,
+                [styles.secondary]: props.secondary,
+                [styles.outlined]: props.outlined,
+                [styles.textOnly]: props.textOnly,
+                [styles.danger]: props.danger,
             })}
-            type={type}
+            type={props.type}
             {...props}
         >
-            {children}
+            {props.children}
         </button>
     );
+}
+Button.defaultProps = {
+    type:'button',
+    children:'default',
+    primary:true
 }
 
 Button.prototype = {
     type: PropTypes.string.isRequired,
-    children: PropTypes.string.isRequired,
+    children: PropTypes.element.isRequired,
     primary: PropTypes.bool,
     secondary: PropTypes.bool,
     textOnly: PropTypes.bool,
     outlined: PropTypes.bool,
+    danger:PropTypes.bool,
 };
