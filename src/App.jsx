90ab41e2d04491d8d43597bab101ca88961f0714
weescoelho <wes.coelho18@gmail.com>
2021-06-30 19:54:16 -0300

    refactor: moved pages components at app to routes folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
 import React from "react";
-import { BrowserRouter, Switch as Routes, Route } from "react-router-dom";
+import Routes from "./routes";
 import "./App.css";
-import EditCategory from "./pages/EditCategory";
-import ExpenseForm from "./pages/ExpenseForm";
-import ListCategories from "./pages/ListCategories";
-import ListExpenses from "./pages/ListExpenses";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/edit-category">
-                    <EditCategory title="Editar Categoria" id={1} />
-                </Route>
-                <Route path="/edit-expense">
-                    <ExpenseForm title="Editar Despesa" isEditing={true} />
-                </Route>
-                <Route path="/">
-                    <ExpenseForm title="Nova Despesa" />
-                </Route>
-                <Route path="/list-category">
-                    <ListCategories title="Despesas" />
-                </Route>
-                <Route path="/list-expenses">
-                    <ListExpenses title="Despesas" group="Casa" />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+       <Routes/>
     );
 }
 
