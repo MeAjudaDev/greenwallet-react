2e9c2383f2c838bd5f6fba8972284b72cbdf63e8
stenioas <stenioas@gmail.com>
2021-07-01 08:29:57 -0300

    chore(button): identation and no newline end of file

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,10 +1,10 @@
-import {screen, render} from '@testing-library/react'
-import '@testing-library/jest-dom';
-import Button from './Button';
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Button from "./Button";
 
-describe('Button test',() => {
-  test('should render button', () => {
-    render(<Button/>)
-    expect(screen.getByText('default')).toBeInTheDocument()
-  })
-})
+describe("Button test", () => {
+    test("should render button", () => {
+        render(<Button />);
+        expect(screen.getByText("default")).toBeInTheDocument();
+    });
+});
