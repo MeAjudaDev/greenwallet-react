2e981eb55c0df592e9fd2a27776dc14801040f1d
angelomarques <anngelo.emanuel07@hotmail.com>
2021-07-03 17:40:51 -0300

    feat(select): adding styles

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,27 +1,29 @@
 import Styles from "./Select.module.scss";
-import arrow from '../../assets/dropdown-icon.svg';
+import arrow from "../../assets/dropdown-icon.svg";
 
-export default function Select({ name }) {
-
-    const options = [
-        { value: 'value1', label: 'label1' },
-        { value: 'value2', label: 'label2' },
-        { value: 'value3', label: 'label3' },
-        { value: 'value4', label: 'label4' },
-        { value: 'value5', label: 'label5' }
-    ]
-
+export default function Select({
+    name,
+    options = [
+        { value: "value1", label: "label1" },
+        { value: "value2", label: "label2" },
+        { value: "value3", label: "label3" },
+        { value: "value4", label: "label4" },
+        { value: "value5", label: "label5" },
+    ],
+}) {
     return (
         <div className={Styles.select}>
-            <select name={name} style={{ backgroundImage: 'url(' + arrow + ') no-repeat right #ddd' }} >
-                {
-                    options.map(option => (
-                        <option value={option.value}>{option.label} </option>
-                    )
-                    )
-                }
+            <select
+                name={name}
+                style={{
+                    backgroundImage: "url(" + arrow + ") no-repeat right #ddd",
+                }}
+            >
+                {options.map((option) => (
+                    <option value={option.value}>{option.label} </option>
+                ))}
             </select>
             <img src={arrow} alt="" />
         </div>
-    )
+    );
 }
