3de82c83a2dfd61019d82620ffb1e315ce0b2d65
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-07-03 17:24:55 -0300

    feat(select): adding styles

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -4,19 +4,24 @@ import arrow from '../../assets/dropdown-icon.svg';
 export default function Select({ name }) {
 
     const options = [
-        {value: 'value1', label: 'label1'},
-        {value: 'value2', label: 'label2'},
-        {value: 'value3', label: 'label3'},
-        {value: 'value4', label: 'label4'},
-        {value: 'value5', label: 'label5'}
+        { value: 'value1', label: 'label1' },
+        { value: 'value2', label: 'label2' },
+        { value: 'value3', label: 'label3' },
+        { value: 'value4', label: 'label4' },
+        { value: 'value5', label: 'label5' }
     ]
 
-    return <select name={name} className={Styles.select} style={{backgroundImage: arrow + 'no-repeat right #ddd'}} >
-       {
-           options.map(option => (
-               <option value={ option.value }>{ option.label } </option>
-            )
-           )
-       }
-    </select>;
+    return (
+        <div className={Styles.select}>
+            <select name={name} style={{ backgroundImage: 'url(' + arrow + ') no-repeat right #ddd' }} >
+                {
+                    options.map(option => (
+                        <option value={option.value}>{option.label} </option>
+                    )
+                    )
+                }
+            </select>
+            <img src={arrow} alt="" />
+        </div>
+    )
 }
