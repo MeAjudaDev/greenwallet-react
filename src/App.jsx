f2b63599276d340268a22a78adf714d674015892
weescoelho <wes.coelho18@gmail.com>
2021-07-03 16:15:04 -0300

    refactor: add routes on app.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
 import React from "react";
 import Routes from "./routes";
 import "./App.css";
-import EditCategory from "./pages/EditCategory";
-import ExpenseForm from "./pages/ExpenseForm";
-import ListCategories from "./pages/ListCategories";
-import ListExpenses from "./pages/ListExpenses";
-import ExportModal from './components/ExportModal';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/edit-category">
-                    <EditCategory title="Editar Categoria" id={1} />
-                </Route>
-                <Route path="/edit-expense">
-                    <ExpenseForm title="Editar Despesa" isEditing={true} />
-                </Route>
-                <Route path="/new-expense">
-                    <ExpenseForm title="Nova Despesa" />
-                </Route>
-                <Route path="/">
-                    <ExportModal/>
-                </Route>
-                <Route path="/list-category">
-                    <ListCategories title="Despesas" />
-                </Route>
-                <Route path="/list-expenses">
-                    <ListExpenses title="Despesas" group="Casa" />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+     <Routes/>
     );
 }
 
