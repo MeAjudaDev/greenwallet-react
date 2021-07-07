5b2c235629620268f16d95bd293e9b82b03acbb2
angelomarques <anngelo.emanuel07@hotmail.com>
2021-07-07 19:39:21 -0300

    refactor(select): delete default options, add an alt attribute to the img on the select

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,29 +1,15 @@
 import Styles from "./Select.module.scss";
 import arrow from "../../assets/dropdown-icon.svg";
 
-export default function Select({
-    name,
-    options = [
-        { value: "value1", label: "label1" },
-        { value: "value2", label: "label2" },
-        { value: "value3", label: "label3" },
-        { value: "value4", label: "label4" },
-        { value: "value5", label: "label5" },
-    ],
-}) {
+export default function Select({ name, options = [] }) {
     return (
         <div className={Styles.select}>
-            <select
-                name={name}
-                style={{
-                    backgroundImage: "url(" + arrow + ") no-repeat right #ddd",
-                }}
-            >
+            <select name={name}>
                 {options.map((option) => (
                     <option value={option.value}>{option.label} </option>
                 ))}
             </select>
-            <img src={arrow} alt="" />
+            <img src={arrow} alt="arrow dropdown icon" />
         </div>
     );
 }
