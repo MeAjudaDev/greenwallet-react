9616d53ec100eacaba83c485fd389dedecd5c355
weescoelho <wes.coelho18@gmail.com>
2021-07-08 17:30:23 -0300

    refactor(header): added hamburguer menu icon and reposition title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import Avatar from '../Avatar';
-
-import styles from './Header.module.scss';
+import Avatar from "../Avatar";
+import menu from "../../assets/menu.svg";
+import styles from "./Header.module.scss";
 
 export default function Header({ title }) {
     return (
         <header className={styles.header}>
-            <span className="material-icons">menu</span>
+            <button className={styles.menuButton}>
+                <img src={menu} alt="" />
+            </button>
             <h1>{title}</h1>
             <Avatar url="/images/avatar.svg" />
         </header>
