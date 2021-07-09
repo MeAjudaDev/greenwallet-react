5183e18ce6eac519f7deb838f444e8380223676c
weescoelho <wes.coelho18@gmail.com>
2021-07-09 12:02:07 -0300

    refactor(header): change import name from hamburger image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import Avatar from "../Avatar";
-import menu from "../../assets/menu.svg";
+import imgMenu from "../../assets/menu.svg";
 import styles from "./Header.module.scss";
 
 export default function Header({ title }) {
     return (
         <header className={styles.header}>
             <button role="menuopen" className={styles.menuButton}>
-                <img src={menu} alt="menu open" />
+                <img src={imgMenu} alt="menu open" />
             </button>
             <h1>{title}</h1>
             <Avatar url="/images/avatar.svg" />
