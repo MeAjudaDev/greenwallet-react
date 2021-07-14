8f6b0a32078afe3fc51ebcc1677bef6f29c8822f
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-07-14 20:06:27 -0300

    refactor(ListExpense): rewriting page - begin

--- src/pages/ListExpenses/ListExpenses.jsx
+++ src/pages/ListExpenses/ListExpenses.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
 import Header from "../../components/Header";
-import MonthsNavBar from "../../components/MonthsNavBar";
 
 import { filterExpenses } from "../../api/v1/expenses";
 import toBRL from "../../utils/formatNumber";
@@ -9,70 +8,41 @@ import toBRL from "../../utils/formatNumber";
 import styles from "./ListExpenses.module.scss";
 
 export default function ListExpenses() {
-    const [expenses, setExpenses] = useState([]);
+    const [totalSums, setTotalSums] = useState([]);
+    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
 
     useEffect(() => {
-        filterExpenses({
-            month: 4,
-        })
-            .then((response) => {
-                if (response.status !== 200) {
-                    throw new Error(response.statusText);
-                }
-                return response.json();
-            })
-            .catch((err) => {
-                console.error(err);
-            })
-            .then((response) => {
-                setExpenses(response);
-            });
+        filterExpenses({ month: 4 }).then((response) => {
+            if (response.status !== 200) {
+                throw new Error(response.statusText);
+            }
+
+            return response.json();
+        }).catch((err) => {
+            console.error(err);
+        }).then((response) => {
+            setTotalSums(response);
+        });
     }, []);
 
-    const months = [
-        {
-            label: "Janeiro",
-            link: "#",
-            selected: false,
-        },
-        {
-            label: "Fevereiro",
-            link: "#",
-            selected: false,
-        },
-        {
-            label: "Março",
-            link: "#",
-            selected: false,
-        },
-        {
-            label: "Abril",
-            link: "#",
-            selected: true,
-        },
-        {
-            label: "Maio",
-            link: "#",
-            selected: false,
-        },
-    ];
+    return <>
+        <Header title="Casa" />
+        <form>
 
-    return (
-        <>
-            <Header title="Casa" />
-            <MonthsNavBar items={months} />
-            <section className={styles.wrapper}>
-                <h2 className={styles.totalValue}>{toBRL(1956)}</h2>
-                <ul className={styles.list}>
-                    {expenses &&
-                        expenses.map((expense) => (
-                            <li key={expense.id} className={styles.listItem}>
-                                <span>{expense.description}</span>
-                                <span>{toBRL(expense.value)}</span>
-                            </li>
-                        ))}
-                </ul>
-            </section>
-        </>
-    );
+        </form>
+        <table>
+            <thead>
+                <th colSpan="2">Total por categoria</th>
+            </thead>
+            <tbody>
+                {totalSums.map(sum => {
+                    <tr>
+                        <td>{sum.category}</td>
+                        <td>{toBRL(sum.amount)}</td>
+                    </tr>
+                })}
+                <tr></tr>
+            </tbody>
+        </table>
+    </>;
 }
