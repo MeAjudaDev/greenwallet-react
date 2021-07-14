80e2c0d0951b7c32b554f1b7a48ff104b4aed3a0
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-07-14 20:46:49 -0300

    refactor(listExpenses): create form of months and years

--- src/pages/ListExpenses/ListExpenses.jsx
+++ src/pages/ListExpenses/ListExpenses.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import Header from "../../components/Header";
+import SelectInput from "../../components/Form/SelectInput";
 
 import { filterExpenses } from "../../api/v1/expenses";
 import toBRL from "../../utils/formatNumber";
@@ -25,10 +26,80 @@ export default function ListExpenses() {
         });
     }, []);
 
+    const months = [
+        {
+            value: 1,
+            label: "Janeiro"
+        },
+
+        {
+            value: 2,
+            label: "Fevereiro"
+        },
+
+        {
+            value: 3,
+            label: "Março"
+        },
+
+        {
+            value: 4,
+            label: "Abril"
+        },
+
+        {
+            value: 5,
+            label: "Maio"
+        },
+
+        {
+            value: 6,
+            label: "Junho"
+        },
+
+        {
+            value: 7,
+            label: "Julho"
+        },
+
+        {
+            value: 8,
+            label: "Agosto"
+        },
+
+        {
+            value: 9,
+            label: "Setembro"
+        },
+
+        {
+            value: 10,
+            label: "Outubro"
+        },
+
+        {
+            value: 11,
+            label: "Novembro"
+        },
+
+        {
+            value: 12,
+            label: "Dezembro"
+        },
+    ];
+
+    const years =
+        [
+
+        ];
+
     return <>
         <Header title="Casa" />
         <form>
-
+            <fieldset>
+                <SelectInput labelText="Mês" name="month" options={months} />
+                <SelectInput labelText="Ano" name="year" />
+            </fieldset>
         </form>
         <table>
             <thead>
@@ -40,6 +111,7 @@ export default function ListExpenses() {
                         <td>{sum.category}</td>
                         <td>{toBRL(sum.amount)}</td>
                     </tr>
+
                 })}
                 <tr></tr>
             </tbody>
