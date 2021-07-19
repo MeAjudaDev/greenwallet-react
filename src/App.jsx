3af2fb4f8a4215a484280d47b3422af1abf44d62
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-07-19 20:32:53 -0300

    fix(Routes): fixes router error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import styles from "./App.module.scss";
 import Header from "./components/Header";
 import Routes from "./routes";
 
-import styles from "./App.module.scss";
-
 function App() {
-    return <>
+    return <BrowserRouter>
         <Header />
         <main className={styles.mainContent}>
             <Routes />
         </main>
-    </>;
+    </BrowserRouter>;
 }
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,22 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import EditCategory from "../pages/EditCategory";
-import ListCategories from "../pages/ListCategories";
 import ExpenseForm from "../pages/ExpenseForm";
+import ListCategories from "../pages/ListCategories";
 import ListExpenses from "../pages/ListExpenses";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/categoria/editar">
-                    <EditCategory title="Editar Categoria" id={1} />
-                </Route>
-                <Route path="/categoria/listar">
-                    <ListCategories title="Categorias" />
-                </Route>
-                <Route path="/despesa/editar">
-                    <ExpenseForm title="Editar Despesa" isEditing={true} />
-                </Route>
-                <Route path="/despesas/listar">
-                    <ListExpenses title="Despesas" group="Casa" />
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    );
+    return <Switch>
+        <Route path="/categoria/editar">
+            <EditCategory title="Editar Categoria" id={1} />
+        </Route>
+        <Route path="/categoria/listar">
+            <ListCategories title="Categorias" />
+        </Route>
+        <Route path="/despesa/editar">
+            <ExpenseForm title="Editar Despesa" isEditing={true} />
+        </Route>
+        <Route path="/despesas/listar">
+            <ListExpenses title="Despesas" group="Casa" />
+        </Route>
+    </Switch>;
 }
