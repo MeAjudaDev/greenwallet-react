f2129b403f1b32b773cf3e1909a86ea7f1009e5a
Iago Rodrigues <yagomello16@gmail.com>
2021-07-22 22:14:06 -0300

    test: add TextInput unit test

---
+++ src/components/Form/TextInput/__tests__/TextInput.test.js
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TextInput from "../TextInput";
+
+test("should render correct label text", () => {
+    render(<TextInput labelText="Valor" />);
+    const spanElement = screen.getByText("Valor");
+    expect(spanElement).toBeInTheDocument();
+});
+
+test("should show correct placeholder text", () => {
+    render(<TextInput placeholder="Insira o valor da conta" />);
+    const inputElement = screen.getByPlaceholderText("Insira o valor da conta");
+    expect(inputElement).toBeInTheDocument();
+});
+
+test("should not have focus when autoFocus prop is not passed in", () => {
+    render(<TextInput />);
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement).not.toHaveFocus();
+});
+
+test("should not have focus when autoFocus prop is false", () => {
+    render(<TextInput autoFocus={false} />);
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement).not.toHaveFocus();
+});
+
+test("should have focus when autoFocus prop is true", () => {
+    render(<TextInput autoFocus />);
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement).toHaveFocus();
+});
+
+test("should update input when user types in", () => {
+    render(<TextInput autoFocus />);
+    const inputElement = screen.getByRole("textbox");
+
+    userEvent.type(inputElement, "25.00");
+
+    expect(inputElement.value).toBe("25.00");
+});
